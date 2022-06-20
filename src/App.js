7dab497ced6b24c62bc1efba0c7592cc020fe2de
joseguti99 <josepe.gutierrezz@gmail.com>
2022-06-20 16:49:00 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import PreciosBerger from "./Components/Home/PreciosBerger";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<NavBar><Home/></NavBar>}/>
         <Route path="/PreciosBerger" element={<NavBar><PreciosBerger/></NavBar>}/>
